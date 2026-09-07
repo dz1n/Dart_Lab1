a64bc6beb4a2a04ab49045187fc256a71d43b1f6
oxsjkw <oxsjkw@gmail.com>
2026-09-07 16:51:56 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,9 @@
-import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
+void main() {
+  String name = 'Стас';
+  int age = 20;
+  double height = 1.75;
+  bool isStudent = true;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+print('Привет, $name! Тебе $age лет.');
+print('Через пять лет тебе будет ${age + 5} лет.');
 }
